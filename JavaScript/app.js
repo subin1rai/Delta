91087b030ecfab204d7575f59216e2aaf7e88023
MR. Subi <bantawasubin@gmail.com>
2024-03-21 15:33:33 +0545

    part 1 & 2

---
+++ JavaScript/app.js
@@ -0,0 +1,30 @@
+
+let a ="greend" 
+if (a === "red"){
+    console.log("Stop the vehicle");
+}
+
+if (a === "yellow"){
+    console.log("ready to go ");
+}
+if (a === "green"){
+    console.log("go ");
+}
+else{
+    console.log("not green");
+}
+
+
+let size = "S"
+if (size === "XL"){
+    console.log("Price is rs 250");
+}
+else if (size === "L"){
+console.log("Price is 200");
+}
+else if (size === "M"){
+    console.log("Price is 100");
+}
+else{
+    console.log("Price is 50");
+}

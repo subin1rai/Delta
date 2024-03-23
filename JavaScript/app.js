48ec0f01a4cc1864b4f71fc33951df0bf8008319
MR. Subi <bantawasubin@gmail.com>
2024-03-23 22:26:54 +0545

    javaScript string

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -28,3 +28,34 @@ else if (size === "M"){
 else{
     console.log("Price is 50");
 }
+
+
+//day printing statement
+
+let day =3
+
+switch (day) {
+    case 1 :
+        console.log("Sunday");
+    break;
+    case 2 :
+        console.log("Monday");
+        break;
+    case 3 :
+        console.log("Tuesday");
+        break;
+    case 4 :
+        console.log("Wednesday");
+        break;
+    case 5 :
+        console.log("Thursday");
+        break;
+    case 6 :
+        console.log("Friday");
+        break;
+    case 7 :
+        console.log("Saturday");
+        break;
+    default :
+    console.log("Invalid day");   
+    }

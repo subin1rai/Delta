7aa1a45d2ad171b1f72129bcbf1e95e9731057f0
MR. Subi <bantawasubin@gmail.com>
2024-03-31 00:03:51 +0545

    function

---
+++ JavaScript/function.js
@@ -0,0 +1,19 @@
+// //
+
+let request = "odd";
+ function getOddorEven(request) {
+    if(request == "odd"){
+        let odd = function (n) {
+            console.log(!(n % 2 == 0));
+            return odd();
+          };
+    }
+  else if(request == "even"){
+    let even = function (n) {
+        console.log(n % 2 == 0);
+        return even();
+      };
+  }
+  else {
+  console.log("wrong request");
+}
